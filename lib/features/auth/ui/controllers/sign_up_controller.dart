2281b869bdc7abc235d6a45b55fe7f3d0eecfed2
mukarrom <mukarrom21@gmail.com>
2025-02-17 21:30:53 +0600

    api updated and signup signin implemented

--- lib/features/auth/ui/controllers/sign_up_controller.dart
+++ lib/features/auth/ui/controllers/sign_up_controller.dart
@@ -3,18 +3,19 @@ import 'package:m_commerce/app/urls.dart';
 import 'package:m_commerce/service/network/network_caller.dart';
 import 'package:m_commerce/service/network/network_response.dart';
 
-class EmailVerificationController extends GetxController {
+class SignUpController extends GetxController {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
+  final String _errorMessage = "";
+  String get errorMessage => _errorMessage;
 
-  Future<bool> verifyEmail(String email) async {
+  Future<bool> signUp(Map<String, dynamic> body) async {
     bool isSuccess = false;
     _isLoading = true;
     update();
-    NetworkResponse response = await Get.find<NetworkCaller>().getRequest(
-      Urls.verifyEmail(email),
-    );
-    if (response.isSuccess) {
+    NetworkResponse response =
+        await Get.find<NetworkCaller>().postRequest(Urls.signUp, body);
+    if (response.statusCode == 200) {
       isSuccess = true;
     }
 
--- lib/features/auth/ui/screens/sign_in_screen.dart
+++ lib/features/auth/ui/screens/sign_in_screen.dart
@@ -1,30 +1,33 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:m_commerce/features/auth/ui/controllers/email_verification_controller.dart';
-import 'package:m_commerce/features/auth/ui/screens/otp_verification_screen.dart';
+import 'package:m_commerce/app/app_colors.dart';
+import 'package:m_commerce/features/auth/ui/controllers/sign_in_controller.dart';
+import 'package:m_commerce/features/auth/ui/screens/sign_up_screen.dart';
 import 'package:m_commerce/features/auth/ui/widgets/app_logo_widget.dart';
+import 'package:m_commerce/features/common/ui/screens/main_layout.dart';
 import 'package:m_commerce/features/common/ui/widgets/center_progress_indicator.dart';
 import 'package:m_commerce/features/common/ui/widgets/snack_bar_message.dart';
 
-class EmailVerificationScreen extends StatefulWidget {
-  static const String name = 'email-verification';
+class SignInScreen extends StatefulWidget {
+  static const String name = 'sign-in';
 
-  const EmailVerificationScreen({super.key});
+  const SignInScreen({super.key});
 
   @override
-  State<EmailVerificationScreen> createState() =>
-      _EmailVerificationScreenState();
+  State<SignInScreen> createState() => _SignInScreenState();
 }
 
-class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
-  final EmailVerificationController _emailVerificationController =
-      Get.find<EmailVerificationController>();
+class _SignInScreenState extends State<SignInScreen> {
+  final SignInController _signInController = Get.find<SignInController>();
   final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -68,7 +71,27 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
                 const SizedBox(
                   height: 16,
                 ),
-                GetBuilder<EmailVerificationController>(builder: (controller) {
+                TextFormField(
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.next,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _passwordTEController,
+                  validator: (value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return "Please enter your password";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                GetBuilder<SignInController>(builder: (controller) {
                   return Visibility(
                     visible: !controller.isLoading,
                     replacement: const CenterProgressIndicator(),
@@ -78,6 +101,7 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
                     ),
                   );
                 }),
+                _buildSignUpSection(textTheme),
               ],
             ),
           ),
@@ -88,20 +112,16 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
 
   Future<void> _onPressedNextButton() async {
     if (_formKey.currentState?.validate() ?? false) {
-      bool isSuccess = await _emailVerificationController
-          .verifyEmail(_emailTEController.text.trim());
+      bool isSuccess = await _signInController.signIn(
+          _emailTEController.text.trim(), _passwordTEController.text);
       if (isSuccess) {
         if (mounted) {
-          showSnackBarMessage(context, "OTP sent to your email address");
           _emailTEController.clear();
           _formKey.currentState?.reset();
-          FocusScope.of(context).unfocus();
           Navigator.pushNamed(
             context,
-            OtpVerificationScreen.name,
-            arguments: _emailTEController.text.trim(),
+            MainLayout.name,
           );
-          print("navigate to otp screen");
         }
       } else {
         if (mounted) {
@@ -109,5 +129,43 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
         }
       }
     }
+  }
+
+  Widget _buildSignUpSection(TextTheme textTheme) {
+    return Center(
+      child: Column(
+        children: [
+          TextButton(
+            onPressed: () => Get.toNamed(SignInScreen.name),
+            child: const Text(
+              'Forgot Password?',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          RichText(
+            text: TextSpan(
+              text: 'Don\'t have an account? ',
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+              children: [
+                TextSpan(
+                  text: ' Sign Up',
+                  style: textTheme.bodyLarge?.copyWith(
+                    color: AppColors.themeColor,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => Get.toNamed(SignUpScreen.name),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
